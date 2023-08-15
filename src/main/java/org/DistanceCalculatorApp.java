44baf96a2d75a1002a71e2d7ee81e86440634fab
timatima <starsinc12@yandex.ru>
2023-08-15 17:57:14 +0400

    initial commit 4

--- src/main/java/org/DistanceCalculatorApp.java
+++ src/main/java/org/DistanceCalculatorApp.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 public class DistanceCalculatorApp {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Dimensional Distance Calculator");
+            JFrame frame = new JFrame("Distance Calculator");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setResizable(false);
             frame.getContentPane().add(new DistanceCalculatorPanel());
